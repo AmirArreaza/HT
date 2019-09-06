93b42abfb1f31c4bbbda90b782bf770e3e3d249e
AmirArreaza <ajaarreazav@gmail.com>
2019-09-06 15:57:54 +0100

    Approved use of spark as a mock server for the for the restful API.
    Including also Gson as requirement by Spark but also to use for json documents

---
+++ src/main/java/com/hometask/main/Main.java
@@ -0,0 +1,16 @@
+package com.hometask.main;
+
+import static spark.Spark.*;
+
+/**
+ *
+ * @author AmirArreaza
+ */
+
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+}
