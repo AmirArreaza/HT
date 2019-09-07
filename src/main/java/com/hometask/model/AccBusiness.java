4f8312c438a459804e73ed3f8b36b2f8dea6cca3
AmirArreaza <ajaarreazav@gmail.com>
2019-09-07 15:14:31 +0100

    Task 001: Models
    Added the business account class for Company and freelancer customers
    
    Added the tests to create a business account

---
+++ src/main/java/com/hometask/model/AccBusiness.java
@@ -0,0 +1,84 @@
+package com.hometask.model;
+
+public class AccBusiness extends Account {
+
+    /**
+     * Business Account types
+     */
+    public enum businessType{
+        COMPANY,
+        FREELANCER
+    }
+
+    /**
+     * Pricing types for a company account
+     */
+    public enum pricingCompany{
+        FREE,
+        GROW,
+        SCALE,
+        ENTERPRISE
+    }
+
+    /**
+     * Pricing types for a freelance account
+     */
+    public enum pricingFreelancer{
+        FREE,
+        PROFESSIONAL
+    }
+
+    /**
+     * Account type
+     */
+    private businessType customerType;
+
+    /**
+     * Pricing of the account
+     */
+    private String pricing;
+
+    /**
+     *
+     * @param accountNumber account number to set
+     * @param sortCode sort code of the account to set
+     * @param customerType customer account type to set
+     */
+    public AccBusiness(String accountNumber, String sortCode, businessType customerType){
+        this.setAccountNumber(accountNumber);
+        this.setSortCode(sortCode);
+        this.customerType = customerType;
+    }
+
+    /**
+     *
+     * @return the account type
+     */
+    public businessType getcustomerType() {
+        return customerType;
+    }
+
+    /**
+     *
+     * @param customerType Customer account type to set
+     */
+    public void setcustomerType(businessType customerType) {
+        this.customerType = customerType;
+    }
+
+    /**
+     *
+     * @return the pricing for the account
+     */
+    public String getPricing() {
+        return pricing;
+    }
+
+    /**
+     *
+     * @param pricing pricing of the account to set
+     */
+    public void setPricing(String pricing) {
+        this.pricing = pricing;
+    }
+}
--- src/test/java/com/hometask/model/AccountTests.java
+++ src/test/java/com/hometask/model/AccountTests.java
@@ -39,6 +39,34 @@ public class AccountTests {
 
     }
 
+    @Test
+    public void createBusinessAccount(){
 
+        AccBusiness companyAccount = new AccBusiness("","", AccBusiness.businessType.COMPANY);
+        AccBusiness freelancerAccount = new AccBusiness("","", AccBusiness.businessType.FREELANCER);
+
+        // Company Account tests
+        Assert.assertEquals("0123456789", companyAccount.getAccountNumber());
+        Assert.assertEquals("00-21-30", companyAccount.getSortCode());
+        Assert.assertEquals("HA9 0GE", companyAccount.getBankAddress().getPostCode());
+        Assert.assertEquals("REVOGB21", companyAccount.getBankIdentifierCode());
+
+        Assert.assertEquals(AccBusiness.businessType.COMPANY, companyAccount.getcustomerType());
+        Assert.assertEquals(20.56, companyAccount.getBalance(), 0);
+        Assert.assertEquals('£', companyAccount.getCurrency());
+        Assert.assertEquals(AccBusiness.pricingCompany.ENTERPRISE, companyAccount.getPricing());
+
+        // Freelancer account tests
+        Assert.assertEquals("0123456789", freelancerAccount.getAccountNumber());
+        Assert.assertEquals("00-21-30", freelancerAccount.getSortCode());
+        Assert.assertEquals("HA9 0GE", freelancerAccount.getBankAddress().getPostCode());
+        Assert.assertEquals("REVOGB21", freelancerAccount.getBankIdentifierCode());
+
+        Assert.assertEquals(AccBusiness.businessType.FREELANCER, freelancerAccount.getcustomerType());
+        Assert.assertEquals(20.56, freelancerAccount.getBalance(), 0);
+        Assert.assertEquals('£', freelancerAccount.getCurrency());
+        Assert.assertEquals(AccBusiness.pricingFreelancer.PROFESSIONAL, freelancerAccount.getPricing());
+
+    }
 
 }
