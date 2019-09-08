6a0d04514a9c380f165ae74b4c7434a5b8b48675
AmirArreaza <ajaarreazav@gmail.com>
2019-09-08 21:07:09 +0100

    Task 002: Controller
    Added the get all implementation to retrieve all customers

--- src/main/java/com/hometask/controller/Controller.java
+++ src/main/java/com/hometask/controller/Controller.java
@@ -2,6 +2,8 @@ package com.hometask.controller;
 
 import com.google.gson.Gson;
 
+import java.util.List;
+
 public interface Controller<E> {
 
     public Gson gson = new Gson();
@@ -9,5 +11,7 @@ public interface Controller<E> {
     boolean add(E e);
 
     boolean edit(E e);
+
+    List<E> getAll();
 
 }
