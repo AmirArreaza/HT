f466ad89486aebb3d0b202f3f214544cb252a8e9
AmirArreaza <ajaarreazav@gmail.com>
2019-09-10 21:22:55 +0100

    Task 002: Controller
    Implemented the method get account

--- src/main/java/com/hometask/controller/AccountCtrl.java
+++ src/main/java/com/hometask/controller/AccountCtrl.java
@@ -3,16 +3,16 @@ package com.hometask.controller;
 import com.hometask.model.AccBusiness;
 import com.hometask.model.AccPersonal;
 import com.hometask.model.Account;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.Random;
 import java.util.TreeMap;
 
 public class AccountCtrl implements Controller<Account>{
 
     public static final TreeMap<String, Account> inMemoryAccounts = new TreeMap<>();
 
-    public AccountCtrl(){populateAccounts();}
+    public AccountCtrl(){populateAccount();}
 
     @Override
     public boolean add(Account account) {
@@ -25,8 +25,8 @@ public class AccountCtrl implements Controller<Account>{
     }
 
     @Override
-    public Account get(String id) {
-        throw new NotImplementedException();
+    public Account get(String number) {
+        return inMemoryAccounts.get(number);
     }
 
     @Override
@@ -34,20 +34,20 @@ public class AccountCtrl implements Controller<Account>{
         return null;
     }
 
-    private void populateAccounts(){
-        AccPersonal personal1 = new AccPersonal("12345","00-00-00");
+    public static void populateAccount(){
+        AccPersonal personal1 = new AccPersonal("123456","00-00-00");
         personal1.addBalance(100.00);
         personal1.settype(AccPersonal.personalTypes.METAL);
-        AccPersonal personal2 = new AccPersonal("23456","00-00-00");
+        AccPersonal personal2 = new AccPersonal("234567","00-00-00");
         personal2.addBalance(123213.30);
         personal2.settype(AccPersonal.personalTypes.METAL);
-        AccPersonal personal3 = new AccPersonal("34567","00-00-00");
+        AccPersonal personal3 = new AccPersonal("345678","00-00-00");
         personal3.addBalance(30.00);
         personal3.settype(AccPersonal.personalTypes.METAL);
-        AccPersonal personal4 = new AccPersonal("45678","00-00-00");
+        AccPersonal personal4 = new AccPersonal("456789","00-00-00");
         personal4.addBalance(450.00);
         personal4.settype(AccPersonal.personalTypes.METAL);
-        AccBusiness personal5 = new AccBusiness("56789","00-00-00", AccBusiness.businessType.FREELANCER);
+        AccBusiness personal5 = new AccBusiness("567890","00-00-00", AccBusiness.businessType.FREELANCER);
         personal5.addBalance(10.00);
         personal5.setPricing(String.valueOf(AccBusiness.pricingFreelancer.PROFESSIONAL));
 
