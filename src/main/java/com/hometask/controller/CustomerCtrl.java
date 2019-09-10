9c93f036fe2267a73b78cdce977bb95303b56f53
AmirArreaza <ajaarreazav@gmail.com>
2019-09-10 22:31:08 +0100

    Task 002: Controller
    Added the transfer code responses to handle the result of the transfer operation

--- src/main/java/com/hometask/controller/CustomerCtrl.java
+++ src/main/java/com/hometask/controller/CustomerCtrl.java
@@ -12,64 +12,20 @@ import java.util.TreeMap;
 
 public class CustomerCtrl implements Controller<Customer>{
 
+    public enum transferResponse{
+        SUCCESS,
+        NOT_ENOUGH_MONEY,
+        ERROR
+    }
+
+    private static AccountCtrl accountCtrl;
+
     public static final TreeMap<String,Customer> inMemoryCustomers = new TreeMap<>();
     public static Customer inMemoryLoggedCustomer;
-    private static AccountCtrl accountCtrl;
 
     public CustomerCtrl(){
         accountCtrl = new AccountCtrl();
         populateCustomers();
-    }
-
-    @Override
-    public boolean add(Customer customer) {
-        try{
-
-            //if(customer.getAccounts().size() <= 0) return false;
-
-            gson.toJson(customer, new FileWriter("Customer.data"));
-
-            return true;
-        }catch(IOException ex){
-            return false;
-        }
-    }
-
-    @Override
-    public boolean edit(Customer customer) {
-        throw new NotImplementedException();
-    }
-
-    @Override
-    public Customer get(String id) {
-        return inMemoryCustomers.get(id);
-    }
-
-    @Override
-    public List<Customer> getAll() {
-        return new ArrayList<>(inMemoryCustomers.values());
-    }
-
-    public boolean logIn(String custId){
-        if(inMemoryCustomers.containsKey(custId)) {
-            Customer customerLoggedIn = inMemoryCustomers.get(custId);
-            inMemoryLoggedCustomer = customerLoggedIn;
-            return true;
-        }
-        return false;
-    }
-
-    public boolean transferMoney(AccPersonal account1, AccPersonal account2, double amount) {
-
-        BigDecimal amountToTransfer = BigDecimal.valueOf(amount).setScale(2);
-        if(account1.getBalance().compareTo(amountToTransfer) <= 0){
-            System.out.println("Not enough money on account");
-            return false;
-        }
-        account1.deductBalance(amount);
-        account2.addBalance(amount);
-
-        return true;
     }
 
     private void populateCustomers() {
@@ -132,5 +88,67 @@ public class CustomerCtrl implements Controller<Customer>{
         inMemoryCustomers.put(customer4.getId().toString(), customer4);
         inMemoryCustomers.put(customer5.getId().toString(), customer5);
     }
+
+    @Override
+    public boolean add(Customer customer) {
+        try{
+
+            //if(customer.getAccounts().size() <= 0) return false;
+
+            gson.toJson(customer, new FileWriter("Customer.data"));
+
+            return true;
+        }catch(IOException ex){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean edit(Customer customer) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public Customer get(String id) {
+        return inMemoryCustomers.get(id);
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        return new ArrayList<>(inMemoryCustomers.values());
+    }
+
+    public boolean logIn(String custId){
+        if(inMemoryCustomers.containsKey(custId)) {
+            Customer customerLoggedIn = inMemoryCustomers.get(custId);
+            inMemoryLoggedCustomer = customerLoggedIn;
+            return true;
+        }
+        return false;
+    }
+
+    public Account getAccount(String accountNumber){
+        for(Account account : inMemoryLoggedCustomer.getAccounts()){
+            if(account.getAccountNumber().equals(accountNumber)){
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public transferResponse transferMoney(AccPersonal account1, AccPersonal account2, double amount) {
+        try{
+            BigDecimal amountToTransfer = BigDecimal.valueOf(amount).setScale(2);
+            if(account1.getBalance().compareTo(amountToTransfer) <= 0){
+                return transferResponse.NOT_ENOUGH_MONEY;
+            }
+            account1.deductBalance(amount);
+            account2.addBalance(amount);
+            return transferResponse.SUCCESS;
+        }catch(Exception ex){
+            return transferResponse.ERROR;
+        }
+    }
+
 
 }
